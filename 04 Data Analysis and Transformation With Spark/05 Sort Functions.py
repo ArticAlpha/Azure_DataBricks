6c403188a16e42ad87dcb7bf78333e2c05ff3b47
sunnynuri.pc@gmail.com <sunnynuri.pc@gmail.com>
2023-01-30 14:58:19 +0000

    using sort functions

---
+++ 04 Data Analysis and Transformation With Spark/05 Sort Functions.py
@@ -0,0 +1,36 @@
+# Databricks notebook source
+countries_path = '/FileStore/tables/countries.csv'
+
+from pyspark.sql.types import StructType , StructField , IntegerType , StringType, DoubleType
+
+countries_schema = StructType([
+    StructField("COUNTRY_ID",IntegerType(),False),
+    StructField("NAME", StringType(), False),
+    StructField("NATIONALITY", StringType(), False),
+    StructField("COUNTRY_CODE",StringType(), False),
+    StructField("ISO_ALPHA2", StringType(), False),
+    StructField("CAPITAL",StringType(),False),
+    StructField("POPULATION",DoubleType(),False),
+    StructField("AREA_KM2",IntegerType(),False),
+    StructField("REGION_ID",IntegerType(),True),
+    StructField("SUB_REGION_ID",IntegerType(),True),
+    StructField("INTERMEDIATE_REGION_ID",IntegerType(),True),
+    StructField("ORGANIZATION_REGION_ID",IntegerType(),True)
+])
+
+# COMMAND ----------
+
+## sorting data
+countries =spark.read.csv(path=countries_path,schema=countries_schema,header=True)
+
+# COMMAND ----------
+
+countries.sort(countries['Population'].asc()).display()
+
+# COMMAND ----------
+
+countries.orderBy(countries['Population'].asc()).display()
+
+# COMMAND ----------
+
+
